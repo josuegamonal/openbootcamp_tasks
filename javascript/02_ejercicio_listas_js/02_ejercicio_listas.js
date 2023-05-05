60edbe9b3b3c30be33c3e15150e8749d3227175b
josuegamonal <69472426+josuegamonal@users.noreply.github.com>
2023-05-05 11:42:40 +0200

    Add files via upload

---
+++ javascript/02_ejercicio_listas_js/02_ejercicio_listas.js
@@ -0,0 +1,14 @@
+
+
+libro_favorito = {
+	titulo: "Alice in Wonderland",
+	autor: "Lewis Carroll",
+	fecha: 1865,
+	url:"https://www.amazon.es/Alicia-pa%C3%ADs-las-maravillas-Cl%C3%A1sicos/dp/8415618719/ref=asc_df_8415618719/?tag=googshopes-21&linkCode=df0&hvadid=257607669746&hvpos=&hvnetw=g&hvrand=8096612718405905184&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9061042&hvtargid=pla-464037376942&psc=1",
+}
+const fecha_nacimiento = new Date("july 5 1979");
+console.log(fecha_nacimiento);
+
+const lista = ["Josué", 43, true, fecha_nacimiento, libro_favorito]
+
+console.log(lista);
